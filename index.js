8f17cec781d9aa5975a50d92d5f26bd78a9095a7
MisiCreate <77699810@qq.com>
2022-01-17 18:45:21 +0800

    初始版本

---
+++ index.js
@@ -0,0 +1,1 @@
+"use strict";window.screenOrientation="sensor_landscape",loadLib("libs/laya.core.js"),loadLib("libs/laya.ani.js"),loadLib("libs/laya.ui.js"),loadLib("libs/laya.d3.js"),loadLib("libs/des.js"),loadLib("libs/laya.physics3D.js"),loadLib("js/bundle.js");
